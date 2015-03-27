c3c7ecea076cdcd1614eed5de3c9642d207e9a98
cismous <cismous@gmail.com>
2015-03-28 00:47:09 +0800

    add require votes module

--- src/socket.io/index.js
+++ src/socket.io/index.js
@@ -153,6 +153,7 @@ function onMessage(socket, payload) {
 function requireModules() {
 	var modules = ['admin', 'categories', 'groups', 'meta', 'modules',
 		'notifications', 'plugins', 'posts', 'topics', 'user'
+		, 'votes'
 	];
 
 	modules.forEach(function(module) {
