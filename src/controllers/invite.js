2596a567732eceaab65b44a059eab21b71984234
CisMous <cismous@gmail.com>
2015-04-02 20:14:08 +0800

    judge joined

--- src/controllers/invite.js
+++ src/controllers/invite.js
@@ -69,6 +69,9 @@ inviteController.list = function (req, res, next) {
 			invite.getInvite(data, next);
 		},
 		function (data, next) {
+			data.invite.map(function (value, index) {
+				data.invite[index].joined = parseInt(value.joined, 10);
+			});
 			data.breadcrumbs = helpers.buildBreadcrumbs([{text: '[[global:header.invite]]', url: '/invite'}]);
 
 			next(null, data);
