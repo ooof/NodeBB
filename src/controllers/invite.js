5607756e57e7b24472041d5f5d0d02266bc12043
CisMous <cismous@gmail.com>
2015-04-23 16:16:18 +0800

    invite sort

--- src/controllers/invite.js
+++ src/controllers/invite.js
@@ -74,9 +74,9 @@ inviteController.list = function (req, res, next) {
 			var inviteData = [];
 
 			data.invite.map(function (value, index) {
-				data.invite[index].joined = !!parseInt(value.joined, 10);
-				data.invite[index].invited = !!parseInt(value.invited, 10);
-				data.invite[index].expired = !!parseInt(value.expired, 10);
+				data.invite[index].joined = parseInt(value.joined, 10);
+				data.invite[index].invited = parseInt(value.invited, 10);
+				data.invite[index].expired = parseInt(value.expired, 10);
 			});
 
 			// 已发邀请
